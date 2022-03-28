b9995221ba5e52896e67cbca0d4a4daca2893ac5
louieiply <louieiply@gmail.com>
2022-03-28 18:04:28 +1030

    Added comment and read.md file details

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Password-Generator
 A Generator that generates password
+## Description
+
+This is a website of Password Generator
+
+
+
+## Table of Contents
+
+-[Installation]
+-[Usage]
+-[Credit]
+-[License]
+-[Feature]
+
+## Installation
+
+There are no steps required for installation. You only require a web browser of the following (Chrome, Brave, Edge, etc.) to open up the webpage to view the content.
+
+## Usage
+![screenshot 1](content/Assets/screenshots/screenshot1.png)
+
+![screenshot 2](content/Assets/screenshots/screenshot2.png)
+
+![screenshot 3](content/Assets/screenshots/screenshot3.png)
+
+![screenshot 4](content/Assets/screenshots/screenshot4.png)
+
+## Credit
+
+Louie Ip, GitHub for storage and hosting
+
+## License
+ 
+blank
+
+## Features
+
+Webpage Generates password as you desire with different Criteria.
+
--- content/Develop/script.js
+++ content/Develop/script.js
@@ -9,14 +9,17 @@ function writePassword() {
 
 
 }
+//Generate password
 function generatePassword(){
   var password = "";
   var confirmed = false;
   var length = 0;
   alert("Following Password criteria:\n-Special Chararcters\n-Uppercase letters\n-Lowercase letters\n-numric letters");
+  //Check password criteria until requirement has met
   while(!confirmed){
     var condition = false;
-    var options = new Map(); 
+    var options = new Map();
+    //get selected criteria
     var symbols = confirm("Password include Special Characters?"); 
     options.set("symbols",symbols);
     var upper = confirm("Password include Uppercase letters ?");
@@ -25,6 +28,7 @@ function generatePassword(){
     options.set("lower",lower);
     var numric = confirm("Password include numric letters ?");
     options.set("numric",numric);
+    //check password length
     if(length === 0){
       while(length < 8 || length > 128){
         length = prompt("Desired Password length (8-128 characters)");
@@ -33,32 +37,40 @@ function generatePassword(){
         }
       }
     }
+    //check criteria needed at least 1 selected
     options.forEach(option => {
       if(option === true){
         condition = true;
         return;
       }
     });
+    //if yes set text to html element
     if(condition){
       var text = "Do you confirm the following list of requirements:\n-Symbols: "+ symbols +"\n-Uppercase Letter: " + upper + "\n-Lowercase Letter: " +lower+ "\n-Numric: "+numric;
       confirmed = confirm(text);
     }
+    //if not alert pop up
     else{
       alert("Please choose at least one of the Password criteria.");
     }
   }
+  //store chosen criteria in array
   var chosen_criteria = new Array();
   options.forEach (function(value, key) {
     if(value === true){
       chosen_criteria.push(key);
     }
   })
+  //Password Generate process
   for(i = 0; i < length; i++){
+    //randomise array index
     var index = Math.floor((Math.random() * (chosen_criteria.length)));
+    //get randomised criteria from array
     var item = chosen_criteria[index];
     console.log(item);
     switch (item) {
       case "symbols":
+        //special character selected
         //33,35,36,37,38,42,64,94 charcode
         var ran = Math.floor(Math.random() * 8);
         if(ran === 0 ){
@@ -78,16 +90,19 @@ function generatePassword(){
         password +=  String.fromCharCode(ran);
         break;
       case "upper":
+        //uppercase letter selected
         //65-90 charcode
         var ran = Math.floor(Math.random() * 26) + 65;
         password +=  String.fromCharCode(ran);
         break;
       case "lower":
+        //lowercase letter selected
         //97-122 charcode
         var ran = Math.floor(Math.random() * 26) + 97;
         password +=  String.fromCharCode(ran);
         break;
       case "numric":
+        //numric letter selected
         //48-57 charcode
         var ran = Math.floor(Math.random() * 10) + 48;
         password +=  String.fromCharCode(ran);
@@ -95,6 +110,7 @@ function generatePassword(){
     }
     
   }
+  //return password
   return password;
 }
 
